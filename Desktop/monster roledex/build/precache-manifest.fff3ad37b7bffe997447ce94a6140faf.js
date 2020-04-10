f9be9c974cc83e79f8ef7295d311adcf9dcd508d
motcondicontrinh1 <hungvtcvip1@gmail.com>
2020-04-10 09:39:49 +0700

    adding

---
+++ Desktop/monster roledex/build/precache-manifest.fff3ad37b7bffe997447ce94a6140faf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98583420c89e06a963d9cbe81087cb4",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "48207d0cd26694b7913c",
+    "url": "/monster-roledex/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5bd8ce374e9a12f13a61",
+    "url": "/monster-roledex/static/css/main.f03ff80a.chunk.css"
+  },
+  {
+    "revision": "48207d0cd26694b7913c",
+    "url": "/monster-roledex/static/js/2.98f9ead5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roledex/static/js/2.98f9ead5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd8ce374e9a12f13a61",
+    "url": "/monster-roledex/static/js/main.8a15f6ce.chunk.js"
+  },
+  {
+    "revision": "23e0c5b597a9d0a7c8aa",
+    "url": "/monster-roledex/static/js/runtime-main.ecf5ee71.js"
+  }
+]);
---
+++ Desktop/monster roledex/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { CardList } from "./components/card-list/card-list.components";
+
+import { SearchBox } from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const filteredMonsters = this.searchFilter();
+    return (
+      <div className="App">
+        <h1>Monster Searcher</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange()}
+        />
+         <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+
+  handleChange() {
+    return (e) => this.setState({ searchField: e.target.value });
+  }
+
+  searchFilter() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    return filteredMonsters;
+  }
+}
